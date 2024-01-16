84f0e1b88098550c68c2cf5ee05b6b985e81e256
ddudnik1977 <13111973@list.ru>
2024-01-16 21:35:38 +0300

    add solution JavaSyntax task 0304

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Scanner.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task03.task0303;
+
+public class Scanner {
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task03.task0303;
+
+import java.util.Scanner;
+
+/* 
+Школа или институт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int age = 1;
+        int ag = 2;
+        int a = 3;
+
+
+        System.out.print(age + " " + ag + " " + a);
+
+        System.out.print(age + " " + " " + a);
+
+
+    }
+}
