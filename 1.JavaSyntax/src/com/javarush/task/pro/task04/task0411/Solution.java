f1d3027814e938889947bef5b45cb6d9cb47ac25
ddudnik1977 <13111973@list.ru>
2024-02-06 13:03:55 +0300

    add solution JavaSyntax task 0411 - 1 Показываем, что получаем

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
@@ -7,7 +7,7 @@ package com.javarush.task.pro.task04.task0411;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        for (int i = 0; i < 15; i++) {
+        for (int i = 1; i < 16; i++) {
             if (i % 2 == 0)
                 System.out.println(i);
         }
